6ef5ab72695aadd2fe015a6510b81c3fbfd46fd0
Veronika Vrbková <veronikavrbkova10@gmail.com>
2025-10-06 21:31:41 +0200

    19 Kontrola palindromu

--- python/tasks/task_19_is_palindrome.py
+++ python/tasks/task_19_is_palindrome.py
@@ -4,4 +4,10 @@ def is_palindrome(s: str) -> bool:
     """
     Vrátí True, pokud je řetězec palindrom.
     """
+    cislo = len(s)
+    
+    for i in range(cislo//2):
+        if s[i] != s[-i-1]:
+            return False
 
+    return True
