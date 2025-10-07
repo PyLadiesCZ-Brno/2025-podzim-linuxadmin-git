f7a41572134205d188dabfc034cd9bb3d12f0527
Aline Laizane <alinelaizane@gmail.com>
2025-10-07 22:59:08 +0200

    Task 13: Aline
    Signed-off-by: Aline Laizane <alinelaizane@gmail.com>

--- python/tasks/task_13_max_in_list.py
+++ python/tasks/task_13_max_in_list.py
@@ -4,4 +4,10 @@ def max_in_list(lst: list[int]) -> int:
     """
     Vrátí největší číslo v seznamu.
     """
-
+    if not lst:
+        raise ValueError("Seznam je prázdný")
+    max_value = lst[0]
+    for num in lst[1:]:
+        if num > max_value:
+            max_value = num
+    return max_value
