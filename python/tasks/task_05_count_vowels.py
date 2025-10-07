9ae115b0bdd7d68e6a4dee352076fbc4493a829a
Luc1et-Fedora <milujememarcipan@seznam.cz>
2025-10-07 10:21:09 +0200

    lt-05 vypracovany ukol

--- python/tasks/task_05_count_vowels.py
+++ python/tasks/task_05_count_vowels.py
@@ -4,4 +4,12 @@ def count_vowels(s: str) -> int:
     """
     Vrátí počet samohlásek v řetězci.
     """
+    vowels = "AEIOUaeiouÁÉÍÓÚáéíóúÄËÏÖÜäëïöü"
 
+    count = 0
+
+    for char in s:
+        if char in vowels:
+            count += 1
+
+    return count
