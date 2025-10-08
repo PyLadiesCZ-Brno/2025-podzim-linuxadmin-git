8eaffcc45eb13faea5f2813d82ca202a82e0f414
Anna Kašíková <anna.kasikova6@gmail.com>
2025-10-08 14:40:29 +0200

    09 Seznam unikátních prvků

--- python/tasks/task_09_unique_elements.py
+++ python/tasks/task_09_unique_elements.py
@@ -4,4 +4,8 @@ def unique_elements(lst: list[int]) -> list[int]:
     """
     Vrátí seznam bez duplicitních prvků, zachová původní pořadí.
     """
-
+    list_b = []
+    for i in lst:
+        if i not in list_b:
+            list_b.append(i)
+    return list_b
