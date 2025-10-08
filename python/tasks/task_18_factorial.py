6a3a06f88e5184e434fa0cd66b3492ba37ee12f6
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-10-08 11:04:41 +0200

    18 Faktoriál n

--- python/tasks/task_18_factorial.py
+++ python/tasks/task_18_factorial.py
@@ -5,3 +5,11 @@ def factorial(n: int) -> int:
     Vrátí faktoriál čísla n (n!).
     """
 
+    if n < 0:
+        raise ValueError("Only positive numbers")
+
+    result = 1
+    for i in range(2, n + 1):  # 0! = 1, 1! = 1
+        result *= i
+
+    return result
