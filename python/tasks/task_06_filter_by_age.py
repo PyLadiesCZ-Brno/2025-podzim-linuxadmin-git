3412b3a3f7af8c866f935ca413f5cbd1c8ac7f83
David <slavicek.david29@gmail.com>
2025-10-08 18:28:44 +0200

    Fix type annotation in task 06

--- python/tasks/task_06_filter_by_age.py
+++ python/tasks/task_06_filter_by_age.py
@@ -1,6 +1,6 @@
 # task_06_filter_by_age.py
 
-def filter_by_age(people: dict):
+def filter_by_age(people: list[dict[str, any]]) -> list[dict[str, any]]:
     """
     Napiš funkci filter_by_age, která vezme seznam slovníků, kde každý slovník
     představuje osobu (s klíči name a age), a vrátí seznam osob, které jsou
