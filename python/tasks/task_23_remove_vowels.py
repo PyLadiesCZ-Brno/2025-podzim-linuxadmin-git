bac55f18a5a686b731438e17bb143a9bd390d1bd
Luci.e.t. <160415575+Luc1et@users.noreply.github.com>
2025-10-12 12:48:47 +0200

    lt-23 vypracovany-ukol (#25)

--- python/tasks/task_23_remove_vowels.py
+++ python/tasks/task_23_remove_vowels.py
@@ -5,3 +5,13 @@ def remove_vowels(s: str) -> str:
     Vrátí řetězec bez samohlásek (a, e, i, o, u).
     """
 
+    vowels = "aieouAEIOUáéíóúůÁÉÍÓÚŮ"
+    letters = []
+
+    for char in s:
+        if char not in vowels:
+            letters.append(char)
+
+    new_word = "".join(letters)
+
+    return new_word
