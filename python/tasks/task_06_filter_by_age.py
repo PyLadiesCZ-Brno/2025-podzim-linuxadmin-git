d734716b73644149566c85db12eef92c51825645
David <slavicek.david29@gmail.com>
2025-10-13 19:32:02 +0200

    Fix: replace any by Any
    Co-authored-by: Martin Zelený <martin.zeleny@volny.cz>

--- python/tasks/task_06_filter_by_age.py
+++ python/tasks/task_06_filter_by_age.py
@@ -1,6 +1,8 @@
 # task_06_filter_by_age.py
 
-def filter_by_age(people: list[dict[str, any]]) -> list[dict[str, any]]:
+from typing import Any
+
+def filter_by_age(people: list[dict[str, Any]]) -> list[dict[str, Any]]:
     """
     Napiš funkci filter_by_age, která vezme seznam slovníků, kde každý slovník
     představuje osobu (s klíči name a age), a vrátí seznam osob, které jsou
