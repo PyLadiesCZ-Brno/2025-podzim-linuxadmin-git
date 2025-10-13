416dbda911db0dd60ba7597a8a7b7eade247e79f
JanaDrazkova <drazkova.j@gmail.com>
2025-10-13 19:15:46 +0200

    10 Flatten list (#19)

--- python/tasks/task_10_flatten_list.py
+++ python/tasks/task_10_flatten_list.py
@@ -5,3 +5,11 @@ def flatten_list(nested: list[list[int]]) -> list[int]:
     Zploští dvourozměrný seznam do jednoho seznamu.
     """
 
+    final_list = []
+    for i in nested:
+        if isinstance(i, int):
+            final_list.append(i)
+        else:
+            final_list.extend(flatten_list(i))
+
+    return final_list
