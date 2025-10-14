b8dbc7698825c5e50bfd6359b52b4b2037882ad8
Luci.e.t. <160415575+Luc1et@users.noreply.github.com>
2025-10-14 21:48:13 +0200

    lt-30 vypracovany-ukol (#28)

--- python/tasks/task_30_remove_duplicates.py
+++ python/tasks/task_30_remove_duplicates.py
@@ -4,4 +4,13 @@ def remove_duplicates(nums: list[int]) -> list[int]:
     """
     Odstraní duplikáty ze seznamu a zachová pořadí.
     """
+    if not isinstance(nums, list):
+        raise TypeError("Argument musí být seznam (list).")
 
+    new_list = []
+
+    for x in nums:
+        if x not in new_list:
+            new_list.append(x)
+
+    return new_list
