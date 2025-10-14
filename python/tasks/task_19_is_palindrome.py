b7731f47032a52c40c62ce9031752b8bb1016b52
Veronika Vrbková <veronikavrbkova10@gmail.com>
2025-10-14 22:35:20 +0200

    19_palindrom_oprava

--- python/tasks/task_19_is_palindrome.py
+++ python/tasks/task_19_is_palindrome.py
@@ -1,13 +1,10 @@
 # task_19_is_palindrome.py
-
 def is_palindrome(s: str) -> bool:
     """
     Vrátí True, pokud je řetězec palindrom.
     """
     cislo = len(s)
-    
     for i in range(cislo//2):
         if s[i] != s[-i-1]:
             return False
-
     return True
