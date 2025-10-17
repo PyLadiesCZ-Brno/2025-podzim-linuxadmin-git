06ef19c307498c1a48e5b12f7bd0d0d20402d24c
Dagmar Vodáková <d.vodakova@seznam.cz>
2025-10-17 19:01:18 +0200

    39 Obrácené samohlásky

--- python/tasks/task_39_reverse_vowels.py
+++ python/tasks/task_39_reverse_vowels.py
@@ -5,4 +5,16 @@ def reverse_vowels(s: str) -> str:
     Vrátí řetězec s přeházenými samohláskami v opačném pořadí.
     Ostatní znaky zůstávají na místě.
     """
+    vowels_lst = []
+    index_lst = []
+    for index, char in enumerate(s):
+        if char in "AEIOUaeiou":
+            index_lst.append(index)
+            vowels_lst.append(char)
+    vowels_lst.reverse()
+    for index, vowel in list(zip(index_lst, vowels_lst)):
+        s = s[:index] + vowel + s[index + 1:]
+    return s
 
+
+print(reverse_vowels("leetcode"))
